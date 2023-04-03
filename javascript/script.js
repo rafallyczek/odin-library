8c81a7ff65cd6482cbdcb61f966913d6003f50ac
rafallyczek <rafal.lyczek@gmail.com>
2023-04-03 16:42:43 +0200

    Add functions for adding new books
    Added functions which let user add new books to library, also
    filled library array with initial books.

--- javascript/script.js
+++ javascript/script.js
@@ -2,8 +2,28 @@
 // Constants and variables
 //
 const form = document.querySelector(".form");
+const cards = document.querySelector(".cards");
 
-let library = [];
+let library = [
+  {
+    title: "Hobbit",
+    author: "J.R.R.Tolkien",
+    pages: 310,
+    description: "This book has no description.",
+  },
+  {
+    title: "The Ravenmaster",
+    author: "Christopher Skaife ",
+    pages: 256,
+    description: "This book has no description.",
+  },
+  {
+    title: "Clean Code",
+    author: "Martin Robert",
+    pages: 464,
+    description: "This book has no description.",
+  },
+];
 
 function Book(title, author, pages, description) {
   this.title = title;
@@ -12,15 +32,76 @@ function Book(title, author, pages, description) {
   this.description = description;
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary(title, author, pages, description) {
+  const book = new Book(title, author, pages, description);
+  library.push(book);
+  clearBooks();
+  displayBooks();
+}
+
+function displayBooks() {
+  for (let i = 0; i < library.length; i++) {
+    const card = document.createElement("div");
+    const title = document.createElement("h3");
+    const author = document.createElement("p");
+    const authorSpan = document.createElement("span");
+    const authorText = document.createTextNode("Author: ");
+    const pages = document.createElement("p");
+    const pagesSpan = document.createElement("span");
+    const pagesText = document.createTextNode("Pages: ");
+    const description = document.createElement("p");
+
+    card.classList.add("card");
+    card.classList.add("bgc-light");
+    card.classList.add("box-shadow");
+
+    title.textContent = library[i].title;
+
+    authorSpan.classList.add("fw-bold");
+    authorSpan.textContent = library[i].author;
+    author.appendChild(authorText);
+    author.appendChild(authorSpan);
+
+    pagesSpan.classList.add("fw-bold");
+    pagesSpan.textContent = library[i].pages;
+    pages.appendChild(pagesText);
+    pages.appendChild(pagesSpan);
+
+    description.textContent = library[i].description;
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(description);
+
+    cards.appendChild(card);
+  }
+}
+
+function clearBooks() {
+  let child = cards.firstElementChild;
+  while (child) {
+    cards.removeChild(child);
+    child = cards.firstElementChild;
+  }
+}
+
+displayBooks();
 
 //
 // Listeners
 //
 form.addEventListener("submit", (e) => {
+  e.preventDefault();
   const errors = document.querySelectorAll(".invalid");
   if (errors.length > 0) {
-    e.preventDefault();
+    return;
+  } else {
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const description = document.getElementById("description").value;
+    addBookToLibrary(title, author, +pages, description);
   }
 });
 
